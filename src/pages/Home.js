3e68e8106424aca11832fadf09fb7b014505b731
Szecsei Gergő <szecseigergodominik@gmail..com>
2022-03-29 20:21:19 +0200

    added footer and cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,27 @@
 import Nav from "../components/Nav";
+import { cardContent } from "../utils/cardContent";
+import Card from "../components/Card";
+import Footer from "../components/Footer";
+
 function Home() {
     return (
         <>
             <Nav />
-            <picture>
-                <source media="(max-width:576px)" srcSet="img/hard-hat-small.jpeg" />
-                <img src="img/hard-hat.jpg" className="img-fluid" alt="Builder" />
-            </picture>
-
+            <div className="position-relative imgContent">
+                <div className="infoBoxContent">
+                    <h2>Álom-építők</h2>
+                    <p>Lorem ipsum valami szöveg ide kell</p>
+                    <button type="button" className="btn btn-outline-success btn-lg">Ajánlatkérés</button>
+                </div>
+                <div className="info-box"></div>
+            </div>
+            <div className="row rowMargin">
+                {cardContent.map(({ imgClassName, cardTitle, cardContent }) => (
+                    <Card key={imgClassName} imgClassName={imgClassName} cardTitle={cardTitle} cardContent={cardContent} />
+                ))
+                }
+            </div>
+            <Footer />
         </>
     );
 }
